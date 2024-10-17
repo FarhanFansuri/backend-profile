c3b54a67906e945bab813e658386ef90d9df1d88
Farhan Fansuri <farhanfansuri2023@gmail.com>
2024-10-17 19:48:09 +0700

    update(project)

---
+++ server.js
@@ -0,0 +1,10 @@
+// environtment config
+require('dotenv').config()
+
+const express = require('express')
+const port = process.env.PORT || 3000
+const app = express()
+
+app.listen(port,()=>{
+    console.log(`server running on port ${port}!!`)
+})
